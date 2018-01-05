1b950fdff997ec659f3badcacd947992da71bb68
Air Data <airdata@abv.bg>
2018-01-05 23:30:33 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 /opt/puppetlabs/bin/puppet cert generate foreman.lab
 /etc/init.d/postgresql start
+sleep 10
 /etc/init.d/foreman start
 /etc/init.d/puppetserver start
 /etc/init.d/foreman-proxy start
